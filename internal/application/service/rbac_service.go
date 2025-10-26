b325c29814eaac6584b92cff1c2efba1a8f9d133
Mohammad Yavari <myavarih@gmail.com>
2025-10-26 17:48:04 +0330

    Revert "fix: deleted rbac_service because complete implementation is in feat/rbac and the file had errors"
    This reverts commit a89ba65bffed6682af70ad69ccafa7613dde2297.

---
+++ internal/application/service/rbac_service.go
@@ -0,0 +1,35 @@
+package service
+
+import (
+	"hona/backend/internal/application/dto/rbac"
+	"hona/backend/internal/domain/entities"
+)
+
+type RBACService struct {
+}
+
+func NewRBACService() *RBACService {
+	return &RBACService{}
+}
+
+func (rs *RBACService) GetRolesResponse(user entities.User) []rbac.RoleResponse {
+	r := make([]rbac.RoleResponse, 0)
+	for _, role := range user.Roles {
+		p := make([]rbac.PermissionResponse, 0)
+		for _, per := range role.Permissions {
+			p = append(p, rbac.PermissionResponse{
+				ID:          per.ID,
+				Name:        per.Type.String(),
+				Description: per.Description,
+				Category:    per.Category.String(),
+			})
+		}
+		r = append(r, rbac.RoleResponse{
+			ID:          role.ID,
+			Name:        role.Type,
+			Description: role.Description,
+			Permissions: p,
+		})
+	}
+	return r
+}
