1cabff16a6bf53df9042a0069f743f1dcd7f1d01
Mohammad Yavari <myavarih@gmail.com>
2025-10-27 11:56:41 +0330

    feat: started Add pet

--- internal/presentation/controllers/v1/user/pet_controller.go
+++ internal/presentation/controllers/v1/user/pet_controller.go
@@ -1,6 +1,21 @@
 package user
 
+import "github.com/gin-gonic/gin"
+
+type UserPetController struct {
+}
+
+func NewUserPetController() *UserPetController {
+	return &UserPetController{}
+}
+
 // TODO: Add Pet
+func (uc *UserPetController) AddPet(ctx *gin.Context) {
+	type AddPetParams struct {
+		Name string `json:"name" validate:"required"`
+		// TODO: handle pic
+	}
+}
 
 // TODO: Update Pet
 
