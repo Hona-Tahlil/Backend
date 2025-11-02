a986c664a65ce123828c43d2591c71edde66da9b
Mohammad Yavari <myavarih@gmail.com>
2025-11-02 22:16:50 +0330

    Fix/test-cors (#14)

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -4,7 +4,9 @@ import (
 	"hona/backend/bootstrap"
 	"hona/backend/internal/presentation/routes"
 	"hona/backend/wire"
+	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -21,6 +23,15 @@ func main() {
 	}
 
 	app.Seeder.DatabaseSeeder.SeedAll()
+
+	ginEngine.Use(cors.New(cors.Config{
+		AllowAllOrigins:  true,
+		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"*"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 
 	routes.SetUpRoutes(ginEngine, app)
 
