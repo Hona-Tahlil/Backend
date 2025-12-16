d16621e1099fd658294a2c887cded8f1c25f68aa
mobinafarzaneh000 <148908752+mobinafarzaneh000@users.noreply.github.com>
2025-12-16 17:30:43 +0330

    Fix/petsitter signup error (#65)

--- internal/infrastructure/persistence/postgres_registration.go
+++ internal/infrastructure/persistence/postgres_registration.go
@@ -34,7 +34,7 @@ func NewPostgresDatabase() *gorm.DB {
 		dbInstance = db
 
 		seeder := seeder.NewDatabaseSeeder(db)
-		seeder.ClearAll()
+		// seeder.ClearAll()
 
 		db.AutoMigrate(
 			&entities.User{},
@@ -55,7 +55,7 @@ func NewPostgresDatabase() *gorm.DB {
 			&entities.Transfer{},
 		)
 
-		// seeder.SeedAll()
+		seeder.SeedAll()
 	})
 
 	return dbInstance
