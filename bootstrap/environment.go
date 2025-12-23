b40057093862a46db14b1c9756c4b477b927f281
mohammad yavari <myavarih@gmail.com>
2025-12-23 21:35:18 +0330

    feat: added user default profile pic and tested

--- bootstrap/environment.go
+++ bootstrap/environment.go
@@ -18,11 +18,12 @@ type Env struct {
 }
 
 type Storage struct {
-	Buckets              Buckets
-	Endpoint             string
-	AccessKey            string
-	SecretKey            string
-	DefaultPetProfileKey string
+	Buckets               Buckets
+	Endpoint              string
+	AccessKey             string
+	SecretKey             string
+	DefaultPetProfileKey  string
+	DefaultUserProfileKey string
 }
 
 type Buckets struct {
@@ -90,7 +91,8 @@ func NewEnv() *Env {
 				PetProfilePic:  os.Getenv("STORAGE_PET_PROFILE_PIC_BUCKET"),
 				UserProfilePic: os.Getenv("STORAGE_USER_PROFILE_PIC_BUCKET"),
 			},
-			DefaultPetProfileKey: os.Getenv("STORAGE_PET_DEFAULT_PROFILE_KEY"),
+			DefaultPetProfileKey:  os.Getenv("STORAGE_PET_DEFAULT_PROFILE_KEY"),
+			DefaultUserProfileKey: os.Getenv("STORAGE_USER_DEFAULT_PROFILE_KEY"),
 		},
 		PrimaryRedis: Redis{
 			Port:      os.Getenv("RDB_PORT"),
