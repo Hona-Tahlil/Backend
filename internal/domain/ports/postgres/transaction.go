d5eb2b6210fcb7fa073ef6ecd4e07e355a5e7562
mohammad yavari <myavarih@gmail.com>
2025-12-23 20:38:20 +0330

    feat: added get wallet info, transactions and transfers

--- internal/domain/ports/postgres/transaction.go
+++ internal/domain/ports/postgres/transaction.go
@@ -1,7 +1,11 @@
 package domainpostgres
 
-import "hona/backend/internal/domain/entities"
+import (
+	"hona/backend/internal/domain/entities"
+	"hona/backend/internal/infrastructure/persistence/repository/postgres"
+)
 
 type TransactionRepository interface {
 	CreateTransaction(transaction *entities.Transaction) error
+	GetTransactionsByWalletID(walletID uint, options *postgres.QueryOptions) ([]entities.Transaction, int64, error)
 }
--- internal/presentation/controllers/v1/user/wallet_controller.go
+++ internal/presentation/controllers/v1/user/wallet_controller.go
@@ -36,3 +36,64 @@ func (wc *UserWalletController) TopUp(ctx *gin.Context) {
 	msg := controllers.Message{}
 	controllers.Respond(ctx, 200, msg, *res)
 }
+
+func (wc *UserWalletController) GetWallet(ctx *gin.Context) {
+	userID := controllers.GetID(ctx)
+
+	res, err := wc.walletService.GetWallet(userID)
+	if err != nil {
+		panic(err)
+	}
+	msg := controllers.Message{}
+	controllers.Respond(ctx, 200, msg, *res)
+}
+
+func (wc *UserWalletController) ListTransfers(ctx *gin.Context) {
+	type Params struct {
+		Page  int `form:"page" validate:"min=1"`
+		Count int `form:"count" validate:"min=1,max=100"`
+	}
+	params := controllers.Receive[Params](ctx)
+	userID := controllers.GetID(ctx)
+	offset, limit := controllers.GetOffsetLimit(params.Page, params.Count)
+
+	info := wallet.HistoryRequest{
+		UserID: userID,
+		Offset: offset,
+		Limit:  limit,
+	}
+
+	items, total, err := wc.walletService.ListTransfers(info)
+	if err != nil {
+		panic(err)
+	}
+	data := controllers.NewPaginatedResponse(items, total, offset, limit)
+
+	msg := controllers.Message{}
+	controllers.Respond(ctx, 200, msg, data)
+}
+
+func (wc *UserWalletController) ListTransactions(ctx *gin.Context) {
+	type Params struct {
+		Page  int `form:"page" validate:"min=1"`
+		Count int `form:"count" validate:"min=1,max=100"`
+	}
+	params := controllers.Receive[Params](ctx)
+	userID := controllers.GetID(ctx)
+	offset, limit := controllers.GetOffsetLimit(params.Page, params.Count)
+
+	info := wallet.HistoryRequest{
+		UserID: userID,
+		Offset: offset,
+		Limit:  limit,
+	}
+
+	items, total, err := wc.walletService.ListTransactions(info)
+	if err != nil {
+		panic(err)
+	}
+	data := controllers.NewPaginatedResponse(items, total, offset, limit)
+
+	msg := controllers.Message{}
+	controllers.Respond(ctx, 200, msg, data)
+}
